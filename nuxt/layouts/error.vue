1bffedc46a738375aff69ce60cb17dd455c1f750
chenzw <656469722@qq.com>
2021-07-09 08:25:49 +0800

    update

--- nuxt/layouts/error.vue
+++ nuxt/layouts/error.vue
@@ -19,9 +19,12 @@
         },
         methods: {
             handleBack() {
-                this.$router.push({
-                    path: '/'
-                })
+                if (window.history.length <= 1) {
+                    this.$router.push({path:'/'})
+                    return false
+                } else {
+                    this.$router.go(-1)
+                }
             }
         }
     }
